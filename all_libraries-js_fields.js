42f394830450f4c34e7fac54efc53a26417f0e7a
Denise Perry <denisebperry@gmail.com>
2021-09-16 22:10:07 -0700

    Create all_libraries-js_fields.js

---
+++ all_libraries-js_fields.js
@@ -0,0 +1,27 @@
+//(possible JS libraries for JS fields must be separate from JS libraries for MD libraries)
+
+
+//When choosing a field to sort by, MD does it alphabetically.
+//Using this function, one can create a JavaScript field that takes
+//the possible values of the sorted field in the order you place importance,
+//with values going from low to high.
+//Note that this version is for use in JavaScript fields only,
+//as no entry is specified. For use in trigger/action/shared scripts,
+//use mySort(), which is defined in all_libraries-misc.js.
+/*
+//Example usage:
+var myList = ["Equipment", "Reusable Item", "Equipable Item", "Consumable", "Unknown"];
+var myFieldName = "Usage";
+sortField(myFieldName, myList);
+*/
+
+function sortField(fieldName, myList)
+{
+    //var myField = field(fieldName);
+  var myField = fieldName;
+  var length = myList.length;
+
+  for (var i = 0; i < length; i++)
+    if(myField == myList[i])
+      return i+1;
+}
