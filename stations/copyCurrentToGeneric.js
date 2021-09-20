e6bc998093d474066733cf4fdd112f55f38323c7
Denise Perry <denisebperry@gmail.com>
2021-09-19 22:53:48 -0700

    Update copyCurrentToGeneric.js

--- stations/copyCurrentToGeneric.js
+++ stations/copyCurrentToGeneric.js
@@ -2,8 +2,16 @@ function copyCurrentToGeneric(e)
 {
   setAllToNull(e);
   let cur = getCurrent(e);
+ let color = e.field("Current Version")[0].field("Spoiler Status color");
+  e.set("Spoiler Status color",color)
   setCurrent(e, cur);
   setGenFieldsToCur(e, cur);
+}
+
+function setColor(e, cur)
+{
+ let color = cur.field("Spoiler Status color");
+  e.set("Spoiler Status color", color)
 }
 
 //To make sure outdated data isn't accidentally kept in
