2455c483d25ff233fe2246518a40c345d6d2d87a
Denise Perry <denisebperry@gmail.com>
2021-09-19 22:34:26 -0700

    Create GetFields.js

---
+++ stations/GetFields.js
@@ -0,0 +1,15 @@
+function getVersionedFields()
+{
+  return ["Station Name
+          "Valid as of",
+          "Flag Color",
+          "Target Station",
+          "Targeted by",
+          "Allies",
+          "Generalized Entry",
+          "Leader",
+          "Members",
+          "Spoiler Status",
+          "Spoiler Status color",
+          "General Key"];
+}
