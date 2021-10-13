aad5a900c125252cc1a7f529b5e46d1ce459045a
Denise Perry <denisebperry@gmail.com>
2021-10-12 17:45:44 -0700

    Update CreateKeyHu.js

--- humans_versioned/CreateKeyHu.js
+++ humans_versioned/CreateKeyHu.js
@@ -4,9 +4,9 @@ function leadingZeros(n)
   return ('000'+n).slice(-3);
 }
 
-function createKeyHu(e)
+function createKey(e)
 {
-  let key1 = getSP(e)
+  let key1 = getSP(e) + "—"
            + getGI(e)
            + getCI(e)
            + getST(e)
@@ -14,6 +14,8 @@ function createKeyHu(e)
            + getCH(e);
 
   let key2 = getGI(e) + getOR(e) + "—" + e.field("Abbreviation");
+
+  //let key = "Char" + field("Order introduced") + "—Chap" + field("Valid as of")[0].field("Chapter #");
 
   if(includes (e.field("Name"), " (copy)"))
     key += "—C";
@@ -25,13 +27,23 @@ function getSP(e)
 {
   log("SP start");
   return "SP" + e.field("Spoiler Sort");
+  //return "—SP" + 0;
 }
 
 function getGI(e)
 {
   log("GI Start");
 
-  return "—GI" + e.field("General Importance Sort");
+  return "GI" + e.field("General Importance Sort");
+/*
+  let myList = ["Main Character", "Important Side Character", "Important in TWSA only", "Briefly Important", "Lucky to have a name"];
+
+  let myFieldName = "General Importance";
+
+  let gSort = (+mySort(myFieldName, myList));
+
+  return "GI" + gSort;
+*/
 }
 
 function getCI(e)
@@ -39,6 +51,7 @@ function getCI(e)
   log("CI start");
 
   return "—CI" + e.field("Current Importance Sort");
+  //return "—CI" + 1;
 }
 
 function getST(e)
@@ -46,6 +59,12 @@ function getST(e)
   log("ST start");
 
   return "—ST" + e.field("Status Sort");
+  /*log("Status = ");
+  log(e.field("Current Status"));
+  var stList = ["Alive", "Unconscious", "Missing", "Unknown", "Dead",];
+  var st = mySort(e.field("Current Status"), stList);
+
+  return "—ST" + st;*/
 }
 
 function getOR(e)
@@ -58,4 +77,7 @@ function getCH(e)
 {
   log("CH start");
   return "—CH" + leadingZeros(e.field("Chapter Sort"));
+  //var ch = e.field("Valid as of")[0].field("Chapter #");
+
+  //return  "—CH" + leadingZeros(ch);
 }
