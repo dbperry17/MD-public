be8975772a4c4f393dc1fd265c58b2a12455ccbe
Denise Perry <denisebperry@gmail.com>
2021-10-12 17:40:54 -0700

    Create UpdateGenericToCurrent.js

---
+++ humans/UpdateGenericToCurrent.js
@@ -0,0 +1,11 @@
+//Action
+//Place of Action: Entry View Card
+/*
+Imported files: 
+ - all_libraries/misc.js
+ - humans/copyCurrentToGenericHu.js
+ - humans/GetFieldsHu.js
+*/
+
+//Used in case a single entry is not updated correctly. Unlikely to be used now that everything is working, but keeping just in case
+copyCurrentToGenericHu(entry());
