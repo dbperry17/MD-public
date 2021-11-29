6dd8bf76d5eaf956bd94ff970b0bbe485c9411f8
Denise Perry <denisebperry@gmail.com>
2021-11-28 23:43:09 -0700

    Update js_fields.js
    Added getTypeEntry () function to work when imported into fields.

--- imported/js_fields.js
+++ imported/js_fields.js
@@ -1,3 +1,7 @@
+/*
+ * Because JavaScript in fields is slightly different than in Library Scripts, this file is to be the only one imported for JavaScript fields, and should never be used in library scripts.
+ */
+
 //When choosing a field to sort by, MD does it alphabetically.
 //Using this function, one can create a JavaScript field that takes the possible values of the sorted field in the order you place importance, with values going from low to high.
 //Note that this version is for use in JavaScript fields only, as no entry is specified. For use in trigger/action/shared scripts, use mySort(), which is defined in all_libraries/misc.js.
@@ -21,7 +25,52 @@ function sortField(fieldName, myList)
   return -1;
 }
 
-function leadingZerosField(n)
+function leadingZeros(n)
 {
   return ('000'+n).slice(-3);
+}
+
+function getTypeEntry()
+{
+  let e = null;
+  let type = field("Type");
+
+  switch(type)
+  {
+    case "Achievement":
+      e = field("Concept (Achievement)")[0];
+      break;
+    case "Attribute":
+      e = field("Concept (Attribute)")[0];
+      break;
+    case "Contract":
+      e = field("Concept (Contract)")[0];
+      break;
+    case "Deus ex Machina":
+      e = field("Concept (Deus ex Machina)")[0];
+      break;
+    case "Item":
+      e = field("Concept (Item)")[0];
+      break;
+    case "Scenario":
+      e = field("Concept (Scenario)")[0];
+      break;
+    case "Skill":
+      e = field("Concept (Skill)")[0];
+      break;
+    case "Station":
+      e = field("Concept (Station)")[0];
+      break;
+    case "Stigma":
+      e = field("Concept (Stigma)")[0];
+      break;
+    case "Term":
+      e = field("Concept (Term)")[0];
+      break;
+    default:
+      message ("No entry found.");
+      break;
+  }
+
+  return e;
 }
