13e2f3efd9721dcc9c052b654ba25e3b8a5ce32d
Denise B Perry <denisebperry@gmail.com>
2022-02-15 20:42:52 -0700

    V2 human fields

--- chapters/GetCurrentEntries.js
+++ chapters/GetCurrentEntries.js
@@ -1,10 +1,3 @@
-//Shared
-/*
-Imported Files:
- - humans/copyCurrentToGenericHu.js
- - stations/copyCurrentToGenericSt.js
-*/
-
 //MD (on Android, at least) has an old version of JavaScript
 //This function is needed as a workaround for functions built in to later versions of JavaScript.
 //Unfortunately, it appears that I failed to note the source of this bit of code after confirming it worked.
@@ -21,40 +14,61 @@ function objectValues(obj) {
 
 function getCurrentEntries(array)
 {
-  const gk = "General Key";
-  const chSo = "Chapter Sort";  
-  //const arrUnique = filterByID(array); //Remove duplicates
+const gk = "General Key";
+const chSo = "Chapter Sort";
 
-  //https://stackoverflow.com/a/53042397
-  //Note: MD uses an old version of JavaScript, so objectValues() function needed to be written, as seen above
-  //This code should go through the list of entries that are NOT labeled "Future Entry", and if it finds an entry with the same "General Key" (which is thus a different version of the same character), it will only add the one with the largest chapter number to filteredArray (that is, the most recent version of a character)
-  let filteredArray = 
-    objectValues(array.reduce((unique, o) =>
-    {
-      if(!unique[o.field(gk)] || (+(o.field(chSo))
-        > +(unique[o.field(gk)].field(chSo))))
-          unique[o.field(gk)] = o;
-      return unique;
-    }, {}));
+const arrUnique = filterByID(array); //Remove duplicates
 
-  return filteredArray;
+//https://stackoverflow.com/a/53042397
+//Note: MD uses an old version of JavaScript, so objectValues() function needed to be written, as seen above
+//This code should go through the list of entries that are NOT labeled "Future Entry", and if it finds an
+//entry with the same "General Key" (which is thus a different version of the same character), it will only
+//add the one with the largest chapter number to filteredArray (that is, the most recent version of a character)
+let filteredArray = objectValues(arrUnique.reduce((unique, o) => {
+  if(!unique[o.field(gk)] || (+(o.field(chSo)) > +(unique[o.field(gk)].field(chSo))))
+    unique[o.field(gk)] = o;
+  return unique;
+}, {}));
+
+return filteredArray;
 }
 
-function setEntriesCurrent(entList, isHuman)
+//I forgot to note where I found the code I used for this, but I believe this function is to remove duplicates from the array,
+//as some entries were added more than once.
+function filterByID(array)
 {
-  //Filter entList so that the only entry for each item is based on the entry with the largest Chapter Sort value (only among entries without the Spoiler Status of "Future Entry")
-  const filteredArray = getCurrentEntries(entList); 
-
-  //Once all current entries are found, copy fields from current versioned entry to generalized entry
-  for (let i in filteredArray)
+let flags = {};
+let retVal = array.filter(function(entry)
+{
+  if (flags[entry.id])
   {
-    //Don't use setLinkedEntrySpoiler() because it results in looping through the list twice (more if used within this loop)
-    let cur = filteredArray [i]!
-    filteredArray[i].set("Spoiler Status", "Current Entry");
-    let gen = cur.field("Generalized Entry")[0];
-    if(isHuman)
-      copyCurrentToGenericHu(gen, cur)
-    else
-      copyCurrentToGenericSt(gen);
+    return false;
   }
+  flags[entry.id] = true;
+  return true;
+});
+
+return retVal;
+}
+
+function setEntriesCurrent(entList)
+{
+//Filter entList so that the only entry for each item is based on the entry with the largest Chapter Sort value
+//(only among entries without the Spoiler Status of "Future Entry")
+const filteredArray = getCurrentEntries(entList); 
+let typeHuman = filteredArray[0].field("eType").equals("Human");
+
+//Once all current entries are found, copy fields
+//from current versioned entry to generalized entry
+for (let i in filteredArray)
+{
+  //Don't use setLinkedEntrySpoiler() because it results in looping
+  //through the list twice (more if used within this loop)
+  filteredArray[i].set("Spoiler Status", "Current Entry");
+  let v = filteredArray[i].field("Generalized Entry")[0];
+  if(typeHuman)
+    copyCurrentToGenericHu(v);
+  else
+    copyCurrentToGenericSt(v);
+}
 }
---
+++ imported/Old/GetFieldsHu.js
@@ -0,0 +1,221 @@
+function getAllFieldsHu()
+{
+  return [{field:"Picture"},
+  {field:"Name"},
+  {field:"Title/Nickname"},
+  {field:"Hangul"},
+  {field:"Abbreviation"},
+  {field:"Listen"},
+  {field:"Current Version"},
+  {field:"Valid as of"},
+  {field:"First Appearance"},
+  {field:"Current Importance"},
+  {field:"General Importance"},
+  {field:"Current Status"},
+  {field:"Last status update"},
+  {field:"Originally in TWSA?"},
+  {field:"Role in TWSA"},
+  {field:"TWSA Notes"},
+  {field:"Relationship to Dokja"},
+  {field:"Personality"},
+  {field:"Other notes"},
+  {field:"Achievements"},
+  {field:"Contracts"},
+  {field:"Age"},
+  {field:"Supporting Constellation"},
+  {field:"Stigma", attr:"Stigma:Level"},
+  {field:"Personal Attributes", attr:"Personal Attributes:Extra notes"},
+  {field:"Personal Skills", attr:"Personal Skills:Level"},
+  {field:"Any stats known?"},
+  {field:"Stamina"},
+  {field:"Strength"},
+  {field:"Agility"},
+  {field:"Mana"},
+  {field:"Unknown distribution"},
+  {field:"Sum of Stats"},
+  {field:"Overall Evaluation"},
+  {field:"Extra Notes"},
+  {field:"Placeholder"},
+  {field:"Main Scenario #4: Flag Color"},
+  {field:"Home Station"},
+  {field:"Important Items"},
+  {field:"Single Use Items", attr:"Single Use Items:Amount owned"},
+  {field:"Liked by"},
+  {field:"Disliked by"},
+  {field:"Neither liked nor disliked by"},
+  {field:"Introduced?"},
+  {field:"Hangul verified?"},
+  {field:"Any Scenario Data?"},
+  {field:"Main Scenario #4 activated?"},
+  {field:"Mission 4: Flag Holder?"},
+  {field:"Entry Description"},
+  {field:"Sorting Key"},
+  {field:"General Key"},
+  {field:"Updated Chapter"},
+  {field:"All Versions"},
+  {field:"General Importance Sort"},
+  {field:"Current Importance Sort"},
+  {field:"Status Sort"},
+  {field:"Order introduced"}];
+}
+
+/*
+//Attributes of fields
+field("Stigma").attr("Level")
+field("Personal Attributes").attr("Extra notes")
+field("Personal Skills").attr("Level")
+field("Single Use Items").attr("Amount owned")
+*/
+function getAttrFieldsHu()
+{
+  return ["Stigma", "Personal Attributes", "Personal Skills", "Single Use Items"];
+}
+
+function getFieldAttrsHu()
+{
+  return ["Level", "Extra notes", "Level", "Amount owned"];
+}
+
+function getVersionFieldsHu()
+{
+  //Field names to copy to another lib
+  const fields=[{field:"Picture"},
+  {field:"Name"},
+  {field:"Title/Nickname"},
+  {field:"Hangul"},
+  {field:"Abbreviation"},
+  {field:"Listen"},
+  {field:"Valid as of"},
+  {field:"First Appearance"},
+  {field:"Current Importance"},
+  {field:"General Importance"},
+  {field:"Status"},
+  {field:"Last status update"},
+  {field:"Originally in TWSA?"},
+  {field:"Role in TWSA"},
+  {field:"TWSA Notes"},
+  {field:"Relationship to Dokja"},
+  {field:"Personality"},
+  {field:"Other notes"},
+  {field:"Achievements"},
+  {field:"Age"},
+  {field:"Supporting Constellation"},
+  {field:"Stigma", attr:"Stigma:Level"},
+  {field:"Personal Attributes", attr:"Personal Attributes:Extra notes"},
+  {field:"Personal Skills", attr:"Personal Skills:Level"},
+  {field:"Any stats known?"},
+  {field:"Stamina"},
+  {field:"Strength"},
+  {field:"Agility"},
+  {field:"Mana"},
+  {field:"Unknown distribution"},
+  {field:"Sum of Stats"},
+  {field:"Overall Evaluation"},
+  {field:"Extra Notes"},
+  {field:"Placeholder"},
+  {field:"Main Scenario #4: Flag Color"},
+  {field:"Home Station"},
+  {field:"Important Items"},
+  {field:"Single Use Items", attr:"Single Use Items:Amount owned"},
+  {field:"Liked by"},
+  {field:"Disliked by"},
+  {field:"Neither liked nor disliked by"},
+  {field:"Any Scenario Data?"},
+  {field:"Main Scenario #4 activated?"},
+  {field:"Mission 4: Flag Holder?"},
+  {field:"Entry Description"},
+  {field:"Custom Description"},
+  {field:"mykey"},
+  {field:"General Key"},
+  {field:"Custom Entry Name"},
+  {field:"Spoiler Status"},
+  {field:"Previous Version"},
+  {field:"Copy?"},
+  {field:"General Importance Sort"},
+  {field:"Current Importance Sort"},
+  {field:"Status Sort"},
+  {field:"Order introduced"},
+  {field:"Chapter Sort"},
+  {field:"Spoiler Sort"}];
+
+    return fields;
+}
+
+function getSortFieldsHu()
+{
+  return ["General Importance Sort", "Current Importance Sort", "Status Sort", "Order Introduced", "Chapter Sort", "Spoiler Sort"];
+}
+
+function getRemovedFieldsHu()
+{
+  return ["Custom Description", "Custom Entry Name", "Spoiler Status", "Copy?", "Chapter Sort", "Spoiler Sort"];
+}
+
+function getChangedFieldsHu()
+{
+  return [{ver:"Status", gen:"Current Status"}];
+//{ver:"mykey", gen:"Sorting Key"},
+//{ver:"Previous Version", gen:"All Versions"}];
+}
+
+function getAddedFieldsHu()
+{
+  return ["Current Version",
+          "Updated Chapter",
+          "Introduced?",
+          "Hangul verified?"];
+}
+
+function getSharedFieldsHu()
+{
+  return ["Picture",
+  "Name",
+  "Title/Nickname",
+  "Hangul",
+  "Abbreviation",
+  "Listen",
+  "Valid as of",
+  "First Appearance",
+  "Current Importance",
+  "General Importance",
+  "Last status update",
+  "Originally in TWSA?",
+  "Role in TWSA",
+  "TWSA Notes",
+  "Relationship to Dokja",
+  "Personality",
+  "Other notes",
+  "Achievements",
+  "Contracts",
+  "Age",
+  "Supporting Constellation",
+  "Stigma",
+  "Personal Attributes",
+  "Personal Skills",
+  "Any stats known?",
+  "Stamina",
+  "Strength",
+  "Agility",
+  "Mana",
+  "Unknown distribution",
+  "Sum of Stats",
+  "Overall Evaluation",
+  "Extra Notes",
+  "Placeholder",
+  "Main Scenario #4: Flag Color",
+  "Home Station",
+  "Important Items",
+  "Single Use Items",
+  "Liked by",
+  "Disliked by",
+  "Neither liked nor disliked by",
+  "Any Scenario Data?",
+  "Main Scenario #4 activated?",
+  "Mission 4: Flag Holder?",
+  "Entry Description",
+  "General Key",
+  "General Importance Sort",
+  "Current Importance Sort",
+  "Status Sort",
+  "Order introduced"];
+}
