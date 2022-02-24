357e845f052df8152b5c9569ea0fda3182f29301
Denise Perry <denisebperry@gmail.com>
2022-02-24 09:05:08 -0700

    Commenting out debug output for now.
    Will remove commented-out parts when I'm sure I won't need them back.

--- imported/copyCurrentToGenericHu.js
+++ imported/copyCurrentToGenericHu.js
@@ -2,36 +2,36 @@
 function updateGenericToCurrentHu(e, argCur)
 {
 
-let getStart = start();
-let getEnd = "";
+//let getStart = start();
+//let getEnd = "";
 	let cur = argCur;
 	if(cur===null || cur===undefined)
-{
+//{
 		cur = getCurrentHu(e);
-getEnd = end(getStart);
-}
+//getEnd = end(getStart);
+//}
 	let flds = getManualFieldsHu();
 
 // let nullStart = start();
 // 	setAllToNullHu(e, flds);
 // let nullEnd =  end(nullStart);
   
-let setCurStart = start();
+//let setCurStart = start();
 	setCurrentHu(e, cur);
-let setCurEnd = end(setCurStart);
+//let setCurEnd = end(setCurStart);
 
-let setFldsStart = start();
+//let setFldsStart = start();
 	setGenFieldsToCurHu(e, cur, flds);
-let setFldsEnd = end(setFldsStart);
+//let setFldsEnd = end(setFldsStart);
 
-if((e.field("General Key")). equals ("GI1—OR008—YJH"))
-{
-  if(!getEnd.equals(""))
-    output ("Junghyeok's getCurrent", getEnd);
-  //output ("Junghyeok's setNull", nullEnd);
-  output ("Junghyeok's setCur", setCurEnd);
-  output ("Junghyeok's setFlds", setFldsEnd);
-}
+// if((e.field("General Key")). equals ("GI1—OR008—YJH"))
+// {
+//   if(!getEnd.equals(""))
+//     output ("Junghyeok's getCurrent", getEnd);
+//   //output ("Junghyeok's setNull", nullEnd);
+//   output ("Junghyeok's setCur", setCurEnd);
+//   output ("Junghyeok's setFlds", setFldsEnd);
+// }
 }
 
 function setAllToNullHu(e, flds)
